4c3228cd7e9e978a2e7c4b5ad1143922d8a8df8c
Lane <lane.mcauley.dev@gmail.com>
2025-08-08 16:29:21 -0400

    Added formatting to index.html Added current snapshot image Forwarded port for access outside local network

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
 import os
+import subprocess
 
 app = Flask(__name__)
 SNAPSHOT_DIR = "static/snapshots"
+CURRENT_SNAPSHOT_DIR = "static/current_snapshot"
 
 @app.route("/")
 def index():
     images = sorted(os.listdir(SNAPSHOT_DIR), reverse=True)
-    return render_template("index.html", images=images)
+    current_image = ""
+    try:
+        subprocess.Popen("/home/lane/grow-tent-monitor/capture_current_image.py", shell=True)
+        current_image = sorted(os.listdir(CURRENT_SNAPSHOT_DIR), reverse=True)
+    except:
+        pass
+    return render_template("index.html", images=images, current_image=current_image)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- capture_current_image.py
+++ capture_current_image.py
@@ -6,7 +6,7 @@ import datetime
 cam = cv2.VideoCapture(0)
 ret, frame = cam.read()
 if ret:
-    filename = f"/home/lane/grow-tent-monitor/static/snapshots/snapshot_{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.jpg"
+    filename = f"/home/lane/grow-tent-monitor/static/current_snapshot/current_snapshot.jpg"
     cv2.imwrite(filename, frame)
     print("Saved", filename)
 else:
